a122bf27d24219a09bd55b8c552c4973d60cf3fa
Houben,Bram B <bram.houben@student.fontys.nl>
2021-03-08 21:07:49 +0100

    new name

--- SolarParkService/src/main/java/com/example/solarparkservice/controller/SolarParkController.java
+++ SolarParkService/src/main/java/com/example/solarparkservice/controller/SolarParkController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/solarpark")
+@RequestMapping("solarpark")
 public class SolarParkController {
 
     @Autowired
--- SolarParkService/src/main/java/com/example/solarparkservice/service/SolarPowerService.java
+++ SolarParkService/src/main/java/com/example/solarparkservice/service/SolarPowerService.java
@@ -3,5 +3,5 @@ package com.example.solarparkservice.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ISolarPowerService implements ISolarParkPower{
+public class SolarPowerService implements ISolarParkPower{
 }
