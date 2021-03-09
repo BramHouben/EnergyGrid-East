b7ae69d90f50c1118ba186e67c0cf3c4dcafac6d
Houben,Bram B <bram.houben@student.fontys.nl>
2021-03-09 15:13:10 +0100

    sonar fix

--- SolarParkService/src/main/java/com/example/solarparkservice/controller/SolarParkController.java
+++ SolarParkService/src/main/java/com/example/solarparkservice/controller/SolarParkController.java
@@ -3,10 +3,8 @@ package com.example.solarparkservice.controller;
 
 import com.example.solarparkservice.service.ISolarParkPower;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpServerErrorException;
 
 @RestController
 @RequestMapping("solarpark")
