02a2c7159c5d8ecc64be9261555ca44d39df5106
imadfetouh <48285772+imadfetouh@users.noreply.github.com>
2021-03-16 12:04:36 +0100

    Update SolarParkService/src/main/java/org/energygrid/east/solarparkservice/rabbit/RabbitConnection.java
    Co-authored-by: Robert Dams <53059207+Robert8034@users.noreply.github.com>

--- SolarParkService/src/main/java/org/energygrid/east/solarparkservice/rabbit/RabbitConnection.java
+++ SolarParkService/src/main/java/org/energygrid/east/solarparkservice/rabbit/RabbitConnection.java
@@ -5,7 +5,7 @@ import com.rabbitmq.client.ConnectionFactory;
 public abstract class RabbitConnection {
 
     protected final ConnectionFactory connectionFactory = new ConnectionFactory();
-    protected final String queue_name = "solarparkservice_queue";
+    protected static final String queueName = "solarparkservice_queue";
 
     public RabbitConnection() {
         connectionFactory.setHost("localhost");
