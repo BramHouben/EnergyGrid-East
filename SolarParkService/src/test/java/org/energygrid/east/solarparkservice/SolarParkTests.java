753f72cfc0d07cda9f9e072f9e1599bac5c48e7c
Houben,Bram B <bram.houben@student.fontys.nl>
2021-03-23 11:59:05 +0100

    extra testing

--- SolarParkService/src/test/java/org/energygrid/east/solarparkservice/SolarParkTests.java
+++ SolarParkService/src/test/java/org/energygrid/east/solarparkservice/SolarParkTests.java
@@ -1,5 +1,6 @@
 package org.energygrid.east.solarparkservice;
 
+import org.energygrid.east.solarparkservice.errormessages.CantAddSolarParkException;
 import org.energygrid.east.solarparkservice.model.SolarPark;
 import org.energygrid.east.solarparkservice.model.dto.AddSolarParkDTO;
 import org.energygrid.east.solarparkservice.repo.ISolarParkRepo;
@@ -13,8 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.geo.Point;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ActiveProfiles("test")
@@ -47,4 +47,31 @@ class SolarParkTests {
         assertEquals(solarPark.getSolarParkName(), solarPark1.getSolarParkName());
 
     }
+
+    @Test
+    void createSolarParkThrowError() {
+        AddSolarParkDTO solarParkDTO = new AddSolarParkDTO(null, 0, new Point(1, 1), "test", "5961AM", "Limburg", 12, 1, 2015);
+
+        assertThrows(CantAddSolarParkException.class, ()-> solarPowerService.addSolarPark(solarParkDTO));
+
+    }
+
+    @Test
+    void createSolarParkThrowError2() {
+        AddSolarParkDTO solarParkDTO = new AddSolarParkDTO("test", 1000, null, "test", "5961AM", "Limburg", 12, 1, 2015);
+
+        assertThrows(CantAddSolarParkException.class, ()-> solarPowerService.addSolarPark(solarParkDTO));
+
+    }
+
+    @Test
+    void getSolarParkTest(){
+        SolarPark solarPark = new SolarPark();
+        solarPark.setSolarParkName("test");
+        when(iSolarParkRepo.getSolarParkBySolarParkName("test")).thenReturn(solarPark);
+
+        SolarPark solarPark1 = iSolarParkRepo.getSolarParkBySolarParkName("test");
+        assertEquals(solarPark.getSolarParkName(), solarPark1.getSolarParkName());
+    }
+
 }
