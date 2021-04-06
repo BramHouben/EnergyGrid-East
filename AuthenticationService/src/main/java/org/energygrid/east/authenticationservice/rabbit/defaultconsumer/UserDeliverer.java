6c855c8144003c840eb844696c8a70549d3e0dd8
Robert8034 <53059207+Robert8034@users.noreply.github.com>
2021-04-06 11:25:56 +0200

    Changes: - Added UserConsumer for AuthenticationService

---
+++ AuthenticationService/src/main/java/org/energygrid/east/authenticationservice/rabbit/defaultconsumer/UserDeliverer.java
@@ -0,0 +1,18 @@
+package org.energygrid.east.authenticationservice.rabbit.defaultconsumer;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Logger;
+
+public class UserDeliverer implements DeliverCallback {
+
+    private final static Logger logger = Logger.getLogger(UserDeliverer.class.getName());
+
+    @Override
+    public void handle(String s, Delivery delivery) throws IOException {
+        String user = new String(delivery.getBody(), StandardCharsets.UTF_8);
+
+    }
+}
