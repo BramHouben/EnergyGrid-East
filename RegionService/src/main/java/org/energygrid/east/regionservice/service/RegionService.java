586671de0d9a4dc0bc0bd4322c3ffe10da1fe25f
Houben,Bram B <bram.houben@student.fontys.nl>
2021-04-08 10:32:33 +0200

    change

--- RegionService/src/main/java/org/energygrid/east/regionservice/service/RegionService.java
+++ RegionService/src/main/java/org/energygrid/east/regionservice/service/RegionService.java
@@ -3,7 +3,6 @@ package org.energygrid.east.regionservice.service;
 import org.energygrid.east.regionservice.model.House;
 import org.energygrid.east.regionservice.repo.IRegionRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,19 +16,20 @@ public class RegionService implements IRegionService {
 
     @Override
     public List<House> getAllHousesProvince(String regionName, long page) {
-        page *=10;
+        page *= 10;
 
         return regionRepo.getAllByRegion(regionName).stream().skip(page).limit(10).collect(Collectors.toList());
     }
 
     @Override
     public List<House> getAllHousesCity(String cityName, long page) {
-        page *=10;
+        page *= 10;
         return regionRepo.getAllByCity(cityName).stream().skip(page).limit(10).collect(Collectors.toList());
     }
 
     @Override
-    public List<House> getAllHousesStreet(String streetName,long page) {
-        return regionRepo.getAllByStreet(streetName);
+    public List<House> getAllHousesStreet(String streetName, long page) {
+        page *= 10;
+        return regionRepo.getAllByStreet(streetName).stream().skip(page).limit(10).collect(Collectors.toList());
     }
 }
