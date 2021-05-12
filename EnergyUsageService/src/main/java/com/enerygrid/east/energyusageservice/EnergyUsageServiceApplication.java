bb41a81a17be394b4c18b87f45d1870eb43839b8
ramonaerts <ramon.aerts@gmail.com>
2021-05-12 17:53:41 +0200

    removed more code smells

--- EnergyUsageService/src/main/java/com/enerygrid/east/energyusageservice/EnergyUsageServiceApplication.java
+++ EnergyUsageService/src/main/java/com/enerygrid/east/energyusageservice/EnergyUsageServiceApplication.java
@@ -2,10 +2,6 @@ package com.enerygrid.east.energyusageservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class EnergyUsageServiceApplication {
