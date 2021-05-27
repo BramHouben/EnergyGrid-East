d4ffe46a7b5da1f916ff2569d5e91ff4b6cdb553
Frits van Lieshout <fritske05@gmail.com>
2021-05-27 09:50:41 +0200

    Added websocket response

--- Websockets-Service/src/main/java/org/energygrid/east/websocketsservice/WebsocketsServiceApplication.java
+++ Websockets-Service/src/main/java/org/energygrid/east/websocketsservice/WebsocketsServiceApplication.java
@@ -3,7 +3,6 @@ package org.energygrid.east.websocketsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class WebsocketsServiceApplication {
 
--- Websockets-Service/src/main/java/org/energygrid/east/websocketsservice/rabbit/Receiver.java
+++ Websockets-Service/src/main/java/org/energygrid/east/websocketsservice/rabbit/Receiver.java
@@ -1,5 +1,8 @@
 package org.energygrid.east.websocketsservice.rabbit;
 
+import org.energygrid.east.websocketsservice.models.EnergyBalanceDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Level;
@@ -8,9 +11,18 @@ import java.util.logging.Logger;
 @Component
 public class Receiver {
 
+    @Autowired
+    SimpMessagingTemplate template;
+
     private static final java.util.logging.Logger logger = Logger.getLogger(Receiver.class.getName());
 
     public void getBalance(String message) {
         logger.log(Level.INFO, message);
+        listen(message);
+    }
+
+    public void listen(String message) {
+        System.out.println("sending websocket message..");
+        template.convertAndSend("/topic", message);
     }
 }
