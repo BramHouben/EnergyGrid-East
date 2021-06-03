3a9bf342ba487126dc8aa8ba47e1b897debff43a
Robert8034 <53059207+Robert8034@users.noreply.github.com>
2021-06-03 09:38:28 +0200

    Added Bad Request when list is empty and removed cross origin annotations

--- SimulationNuclearService/src/main/java/org/energygrid/east/simulationnuclearservice/controller/ScenarioNuclearController.java
+++ SimulationNuclearService/src/main/java/org/energygrid/east/simulationnuclearservice/controller/ScenarioNuclearController.java
@@ -8,9 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("scenario/nuclear")
 public class ScenarioNuclearController {
@@ -38,6 +38,11 @@ public class ScenarioNuclearController {
     @GetMapping("/latest")
     public ResponseEntity<List<ScenarioExpectationResult>> getLatestScenarios() {
         var result = scenarioNuclearService.getLatestScenarios();
-        return ResponseEntity.ok(result);
+
+        if (!result.isEmpty()) {
+            return ResponseEntity.ok(result);
+        }
+
+        return ResponseEntity.badRequest().build();
     }
 }
