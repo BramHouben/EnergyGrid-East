f9baa5c208b9b05a15c55d5d8e27ac2ef6ce8227
Houben,Bram B <bram.houben@student.fontys.nl>
2021-06-15 09:37:11 +0200

    new pipeline websockets

--- EnergyBalanceService/src/test/java/org/energygrid/east/energybalanceservice/BalanceIntegrationTest.java
+++ EnergyBalanceService/src/test/java/org/energygrid/east/energybalanceservice/BalanceIntegrationTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -39,7 +40,7 @@ class BalanceIntegrationTest {
 
     @BeforeEach
     void setup() {
-        initMocks(this);
+        MockitoAnnotations.openMocks(this);
         this.mockMvc = MockMvcBuilders.standaloneSetup(energyBalanceController).build();
     }
 
